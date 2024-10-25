d29cce87cd673f435cb73f780bf64f682c013f4d
Yunkon Kim <hermitkim1@gmail.com>
2024-10-25 14:41:47 +0900

    Update install-tofu.sh

--- scripts/install-tofu.sh
+++ scripts/install-tofu.sh
@@ -15,16 +15,22 @@ apt-get install -y apt-transport-https ca-certificates curl gnupg git
 
 # Set up the OpenTofu repository
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://get.opentofu.org/opentofu.gpg | sudo tee /etc/apt/keyrings/opentofu.gpg >/dev/null
-curl -fsSL https://packages.opentofu.org/opentofu/tofu/gpgkey | sudo gpg --no-tty --batch --dearmor -o /etc/apt/keyrings/opentofu-repo.gpg >/dev/null
+curl -fsSL https://get.opentofu.org/opentofu.gpg | tee /etc/apt/keyrings/opentofu.gpg >/dev/null
+curl -fsSL https://packages.opentofu.org/opentofu/tofu/gpgkey | gpg --no-tty --batch --dearmor -o /etc/apt/keyrings/opentofu-repo.gpg >/dev/null
 chmod a+r /etc/apt/keyrings/opentofu.gpg /etc/apt/keyrings/opentofu-repo.gpg
 
 # Install OpenTofu
 apt-get update -y
-apt-get install -y tofu=${TOFU_VERSION}
+if ! apt-get install -y tofu=${TOFU_VERSION}; then
+    echo "Failed to install OpenTofu version ${TOFU_VERSION}"
+    exit 1
+fi
 
 # Check tofu version after installation
-tofu --version || { echo "Failed to retrieve OpenTofu version"; exit 1; }
+if ! tofu --version; then
+    echo "Failed to retrieve OpenTofu version"
+    exit 1
+fi
 
 # Remove the installer:
-rm -f install-opentofu.sh
+rm -f install-tofu.sh
