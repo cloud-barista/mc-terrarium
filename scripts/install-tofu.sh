9c69e8aecdd232c9d11c8ec94b27d7688bb0c331
Yunkon Kim <hermitkim1@gmail.com>
2024-10-25 14:51:05 +0900

    Update install-tofu.sh

--- scripts/install-tofu.sh
+++ scripts/install-tofu.sh
@@ -12,15 +12,19 @@ apt-get update -y
 # Ensure that you have installed the dependencies, such as `gnupg`, `software-properties-common`, `curl`, and unzip packages.
 apt-get install -y apt-transport-https ca-certificates curl gnupg git apt-utils
 
-
 # Set up the OpenTofu repository
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://get.opentofu.org/opentofu.gpg | tee /etc/apt/keyrings/opentofu.gpg >/dev/null
 curl -fsSL https://packages.opentofu.org/opentofu/tofu/gpgkey | gpg --no-tty --batch --dearmor -o /etc/apt/keyrings/opentofu-repo.gpg >/dev/null
-chmod a+r /etc/apt/keyrings/opentofu.gpg /etc/apt/keyrings/opentofu-repo.gpg
+# chmod a+r /etc/apt/keyrings/opentofu.gpg /etc/apt/keyrings/opentofu-repo.gpg
+
+# Add the OpenTofu repository to sources list
+echo "deb [signed-by=/etc/apt/keyrings/opentofu-repo.gpg] https://packages.opentofu.org/opentofu/tofu/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/opentofu.list
+
+# Update package list after adding the repository
+apt-get update -y
 
 # Install OpenTofu
-apt-get update -y
 if ! apt-get install -y tofu=${TOFU_VERSION}; then
     echo "Failed to install OpenTofu version ${TOFU_VERSION}"
     exit 1
@@ -32,5 +36,3 @@ if ! tofu --version; then
     exit 1
 fi
 
-# Remove the installer:
-rm -f install-tofu.sh
