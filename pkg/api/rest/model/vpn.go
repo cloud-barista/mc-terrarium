9c6a8fef34e7ca44af6169895f683b162629457c
Yunkon Kim <hermitkim1@gmail.com>
2024-10-29 23:40:24 +0900

    Staging v0.0.10

--- pkg/api/rest/model/vpn.go
+++ pkg/api/rest/model/vpn.go
@@ -23,3 +23,132 @@ type TfVarsGcpAzureVpnTunnel struct {
 	// AzureSubnetName             string `json:"azure-subnet-name" default:"tr-azure-subnet-0"`
 	// GcpVpcSubnetworkName    string `json:"gcp-vpc-subnetwork-name" default:"tr-gcp-subnet-1"`
 }
+
+type OutputGcpAwsVpnInfo struct {
+	Terrarium struct {
+		ID string `json:"id"`
+	} `json:"terrarium"`
+	AWS struct {
+		VpnGateway struct {
+			ResourceType string `json:"resource_type"`
+			Name         string `json:"name"`
+			ID           string `json:"id"`
+			VpcID        string `json:"vpc_id"`
+		} `json:"vpn_gateway"`
+		CustomerGateways []struct {
+			ResourceType string `json:"resource_type"`
+			Name         string `json:"name"`
+			ID           string `json:"id"`
+			IPAddress    string `json:"ip_address"`
+			BgpAsn       string `json:"bgp_asn"`
+		} `json:"customer_gateways"`
+		VpnConnections []struct {
+			ResourceType   string `json:"resource_type"`
+			Name           string `json:"name"`
+			ID             string `json:"id"`
+			Tunnel1Address string `json:"tunnel1_address"`
+			Tunnel2Address string `json:"tunnel2_address"`
+		} `json:"vpn_connections"`
+	} `json:"aws"`
+	GCP struct {
+		Router struct {
+			ResourceType string `json:"resource_type"`
+			Name         string `json:"name"`
+			ID           string `json:"id"`
+			Network      string `json:"network"`
+			BgpAsn       int    `json:"bgp_asn"`
+		} `json:"router"`
+		HaVpnGateway struct {
+			ResourceType string   `json:"resource_type"`
+			Name         string   `json:"name"`
+			ID           string   `json:"id"`
+			Network      string   `json:"network"`
+			IPAddresses  []string `json:"ip_addresses"`
+		} `json:"ha_vpn_gateway"`
+		VpnTunnels []struct {
+			ResourceType string `json:"resource_type"`
+			Name         string `json:"name"`
+			ID           string `json:"id"`
+			IkeVersion   int    `json:"ike_version"`
+			Interface    int    `json:"interface"`
+		} `json:"vpn_tunnels"`
+	} `json:"gcp"`
+}
+
+type OutputGcpAzureVpnInfo struct {
+	Terrarium struct {
+		ID string `json:"id"`
+	} `json:"terrarium"`
+	Azure struct {
+		VirtualNetworkGateway struct {
+			ResourceType string `json:"resource_type"`
+			Name         string `json:"name"`
+			ID           string `json:"id"`
+			Location     string `json:"location"`
+			VpnType      string `json:"vpn_type"`
+			Sku          string `json:"sku"`
+			BgpSettings  struct {
+				ASN              int `json:"asn"`
+				PeeringAddresses []struct {
+					IPAddressConfig string `json:"ip_configuration"`
+					Address         string `json:"address"`
+				} `json:"peering_addresses"`
+			} `json:"bgp_settings"`
+		} `json:"virtual_network_gateway"`
+		PublicIPs struct {
+			IP1 struct {
+				ResourceType string `json:"resource_type"`
+				Name         string `json:"name"`
+				ID           string `json:"id"`
+				IPAddress    string `json:"ip_address"`
+			} `json:"ip1"`
+			IP2 struct {
+				ResourceType string `json:"resource_type"`
+				Name         string `json:"name"`
+				ID           string `json:"id"`
+				IPAddress    string `json:"ip_address"`
+			} `json:"ip2"`
+		} `json:"public_ips"`
+		Connections []struct {
+			ResourceType string `json:"resource_type"`
+			Name         string `json:"name"`
+			ID           string `json:"id"`
+			Type         string `json:"type"`
+			EnableBgp    bool   `json:"enable_bgp"`
+		} `json:"connections"`
+	} `json:"azure"`
+	GCP struct {
+		Router struct {
+			ResourceType string `json:"resource_type"`
+			Name         string `json:"name"`
+			ID           string `json:"id"`
+			Network      string `json:"network"`
+			Bgp          struct {
+				ASN           int    `json:"asn"`
+				AdvertiseMode string `json:"advertise_mode"`
+			} `json:"bgp"`
+		} `json:"router"`
+		HaVpnGateway struct {
+			ResourceType string `json:"resource_type"`
+			Name         string `json:"name"`
+			ID           string `json:"id"`
+			Network      string `json:"network"`
+			Interfaces   []struct {
+			} `json:"interfaces"`
+		} `json:"ha_vpn_gateway"`
+		VpnTunnels []struct {
+			ResourceType string `json:"resource_type"`
+			Name         string `json:"name"`
+			ID           string `json:"id"`
+			Router       string `json:"router"`
+			Interface    int    `json:"interface"`
+		} `json:"vpn_tunnels"`
+		BgpPeers []struct {
+			ResourceType  string `json:"resource_type"`
+			Name          string `json:"name"`
+			PeerIP        string `json:"peer_ip"`
+			PeerASN       int    `json:"peer_asn"`
+			InterfaceName string `json:"interface_name"`
+		} `json:"bgp_peers"`
+	} `json:"gcp"`
+}
