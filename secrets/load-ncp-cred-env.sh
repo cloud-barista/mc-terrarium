4a16b18311293fba71dcd02cc5ce0453a538fa99
Yunkon Kim <hermitkim1@gmail.com>
2024-11-19 16:28:22 +0900

    Update a script to load ncp credential as envs

--- secrets/load-ncp-cred-env.sh
+++ secrets/load-ncp-cred-env.sh
@@ -4,14 +4,19 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ENV_FILE="$SCRIPT_DIR/credential-ncp.env"
 
+echo "ENV_FILE: $ENV_FILE"
+
 # Check if .env file exists
 if [[ -f "$ENV_FILE" ]]; then
-    while IFS= read -r line; do
+    sed -i 's/\r$//' "$ENV_FILE"  # Remove carriage return characters
+    while IFS= read -r line || [[ -n "$line" ]]; do
+        # Skip empty lines and comments
         if [[ -n "$line" && ! "$line" =~ ^# ]]; then
-            eval export "$line"
+            # echo "Processing line: $line"  # Debugging line
+            export "$line"            
         fi
     done < "$ENV_FILE"
     echo "successfully loaded the NCP credential environment variables"
 else
-    echo "error: dose not exist file, $ENV_FILE"
+    echo "error: does not exist file, $ENV_FILE"
 fi
