a62e5e7a1ec5f8e045deb3c51aafdb5bfdbba178
Yunkon Kim <hermitkim1@gmail.com>
2024-11-28 21:18:28 +0900

    Add AWS MQ (ActiveMQ) example

--- templates/vpn/gcp-aws/output.tf
+++ templates/vpn/gcp-aws/output.tf
@@ -96,38 +96,38 @@ output "vpn_info" {
     aws = {
       vpn_gateway = {
         resource_type = "aws_vpn_gateway"
-        name         = aws_vpn_gateway.vpn_gw.tags.Name
-        id           = aws_vpn_gateway.vpn_gw.id
-        vpc_id       = aws_vpn_gateway.vpn_gw.vpc_id
+        name          = aws_vpn_gateway.vpn_gw.tags.Name
+        id            = aws_vpn_gateway.vpn_gw.id
+        vpc_id        = aws_vpn_gateway.vpn_gw.vpc_id
       }
       customer_gateways = [
         {
           resource_type = "aws_customer_gateway"
-          name         = aws_customer_gateway.cgw_1.tags.Name
-          id           = aws_customer_gateway.cgw_1.id
-          ip_address   = aws_customer_gateway.cgw_1.ip_address
-          bgp_asn      = aws_customer_gateway.cgw_1.bgp_asn
+          name          = aws_customer_gateway.cgw_1.tags.Name
+          id            = aws_customer_gateway.cgw_1.id
+          ip_address    = aws_customer_gateway.cgw_1.ip_address
+          bgp_asn       = aws_customer_gateway.cgw_1.bgp_asn
         },
         {
           resource_type = "aws_customer_gateway"
-          name         = aws_customer_gateway.cgw_2.tags.Name
-          id           = aws_customer_gateway.cgw_2.id
-          ip_address   = aws_customer_gateway.cgw_2.ip_address
-          bgp_asn      = aws_customer_gateway.cgw_2.bgp_asn
+          name          = aws_customer_gateway.cgw_2.tags.Name
+          id            = aws_customer_gateway.cgw_2.id
+          ip_address    = aws_customer_gateway.cgw_2.ip_address
+          bgp_asn       = aws_customer_gateway.cgw_2.bgp_asn
         }
       ]
       vpn_connections = [
         {
-          resource_type = "aws_vpn_connection"
-          name         = aws_vpn_connection.vpn_cnx_1.tags.Name
-          id           = aws_vpn_connection.vpn_cnx_1.id
+          resource_type   = "aws_vpn_connection"
+          name            = aws_vpn_connection.vpn_cnx_1.tags.Name
+          id              = aws_vpn_connection.vpn_cnx_1.id
           tunnel1_address = aws_vpn_connection.vpn_cnx_1.tunnel1_address
           tunnel2_address = aws_vpn_connection.vpn_cnx_1.tunnel2_address
         },
         {
-          resource_type = "aws_vpn_connection"
-          name         = aws_vpn_connection.vpn_cnx_2.tags.Name
-          id           = aws_vpn_connection.vpn_cnx_2.id
+          resource_type   = "aws_vpn_connection"
+          name            = aws_vpn_connection.vpn_cnx_2.tags.Name
+          id              = aws_vpn_connection.vpn_cnx_2.id
           tunnel1_address = aws_vpn_connection.vpn_cnx_2.tunnel1_address
           tunnel2_address = aws_vpn_connection.vpn_cnx_2.tunnel2_address
         }
@@ -136,46 +136,46 @@ output "vpn_info" {
     gcp = {
       router = {
         resource_type = "google_compute_router"
-        name         = google_compute_router.router_1.name
-        id           = google_compute_router.router_1.id
-        network      = google_compute_router.router_1.network
-        bgp_asn      = google_compute_router.router_1.bgp[0].asn
+        name          = google_compute_router.router_1.name
+        id            = google_compute_router.router_1.id
+        network       = google_compute_router.router_1.network
+        bgp_asn       = google_compute_router.router_1.bgp[0].asn
       }
       ha_vpn_gateway = {
         resource_type = "google_compute_ha_vpn_gateway"
-        name         = google_compute_ha_vpn_gateway.ha_vpn_gw_1.name
-        id           = google_compute_ha_vpn_gateway.ha_vpn_gw_1.id
-        network      = google_compute_ha_vpn_gateway.ha_vpn_gw_1.network
-        ip_addresses = google_compute_ha_vpn_gateway.ha_vpn_gw_1.vpn_interfaces[*].ip_address
+        name          = google_compute_ha_vpn_gateway.ha_vpn_gw_1.name
+        id            = google_compute_ha_vpn_gateway.ha_vpn_gw_1.id
+        network       = google_compute_ha_vpn_gateway.ha_vpn_gw_1.network
+        ip_addresses  = google_compute_ha_vpn_gateway.ha_vpn_gw_1.vpn_interfaces[*].ip_address
       }
       vpn_tunnels = [
         {
           resource_type = "google_compute_vpn_tunnel"
-          name         = google_compute_vpn_tunnel.vpn_tunnel_1.name
-          id           = google_compute_vpn_tunnel.vpn_tunnel_1.id
-          ike_version  = google_compute_vpn_tunnel.vpn_tunnel_1.ike_version
-          interface    = google_compute_vpn_tunnel.vpn_tunnel_1.vpn_gateway_interface
+          name          = google_compute_vpn_tunnel.vpn_tunnel_1.name
+          id            = google_compute_vpn_tunnel.vpn_tunnel_1.id
+          ike_version   = google_compute_vpn_tunnel.vpn_tunnel_1.ike_version
+          interface     = google_compute_vpn_tunnel.vpn_tunnel_1.vpn_gateway_interface
         },
         {
           resource_type = "google_compute_vpn_tunnel"
-          name         = google_compute_vpn_tunnel.vpn_tunnel_2.name
-          id           = google_compute_vpn_tunnel.vpn_tunnel_2.id
-          ike_version  = google_compute_vpn_tunnel.vpn_tunnel_2.ike_version
-          interface    = google_compute_vpn_tunnel.vpn_tunnel_2.vpn_gateway_interface
+          name          = google_compute_vpn_tunnel.vpn_tunnel_2.name
+          id            = google_compute_vpn_tunnel.vpn_tunnel_2.id
+          ike_version   = google_compute_vpn_tunnel.vpn_tunnel_2.ike_version
+          interface     = google_compute_vpn_tunnel.vpn_tunnel_2.vpn_gateway_interface
         },
         {
           resource_type = "google_compute_vpn_tunnel"
-          name         = google_compute_vpn_tunnel.vpn_tunnel_3.name
-          id           = google_compute_vpn_tunnel.vpn_tunnel_3.id
-          ike_version  = google_compute_vpn_tunnel.vpn_tunnel_3.ike_version
-          interface    = google_compute_vpn_tunnel.vpn_tunnel_3.vpn_gateway_interface
+          name          = google_compute_vpn_tunnel.vpn_tunnel_3.name
+          id            = google_compute_vpn_tunnel.vpn_tunnel_3.id
+          ike_version   = google_compute_vpn_tunnel.vpn_tunnel_3.ike_version
+          interface     = google_compute_vpn_tunnel.vpn_tunnel_3.vpn_gateway_interface
         },
         {
           resource_type = "google_compute_vpn_tunnel"
-          name         = google_compute_vpn_tunnel.vpn_tunnel_4.name
-          id           = google_compute_vpn_tunnel.vpn_tunnel_4.id
-          ike_version  = google_compute_vpn_tunnel.vpn_tunnel_4.ike_version
-          interface    = google_compute_vpn_tunnel.vpn_tunnel_4.vpn_gateway_interface
+          name          = google_compute_vpn_tunnel.vpn_tunnel_4.name
+          id            = google_compute_vpn_tunnel.vpn_tunnel_4.id
+          ike_version   = google_compute_vpn_tunnel.vpn_tunnel_4.ike_version
+          interface     = google_compute_vpn_tunnel.vpn_tunnel_4.vpn_gateway_interface
         }
       ]
     }
