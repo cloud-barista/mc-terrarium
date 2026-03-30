34525b7ed8017d169235b921e2c85016944c8d58
Yunkon Kim <hermitkim1@gmail.com>
2026-03-30 21:51:35 +0900

    release: staging v0.1.3

--- api/docs.go
+++ api/docs.go
@@ -5518,7 +5518,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "v0.1.2",
+	Version:          "v0.1.3",
 	Host:             "localhost:8055",
 	BasePath:         "/terrarium",
 	Schemes:          []string{},
